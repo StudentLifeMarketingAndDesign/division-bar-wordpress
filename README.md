9aba03bc2686974ec05df835ae7d96566253c392
Dustin Quam <quamsta@gmail.com>
2013-06-18 14:18:23 -0500

    initial commit from wrac

---
+++ README.md
@@ -0,0 +1,9 @@
+Division Bar for Wordpress
+==============================
+
+Instructions:
+-------------
+
+* Put the division-bar/ folder inside your theme folder.
+* In your theme header/footer, make sure that you're linking to both the CSS and JS files located in division-bar/
+* Put <?php include 'division-bar/templates/DivisionBar.html' ?> directly after the body tag.
---
+++ division-bar/js/division-bar.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function ($) {
+
+
+    $('.search-toggle').click(function() {
+        $(this).toggleClass('active');
+        $('.division-search').slideToggle();
+        return false;
+    });
+
+	$('.directory-toggle').click(function() {
+		$(this).toggleClass("active");
+		$('.division-directory').slideToggle();
+		return false;
+	});
+
+    $('.division-menu').on('click', '.has-subnav a', function() {
+        $(this).next().slideToggle('slow');
+        $(this).toggleClass('active');
+
+    });
+
+});
