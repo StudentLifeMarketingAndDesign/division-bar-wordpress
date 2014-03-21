46ddf97a975b26c45fe5cfcead11a05b4ff8ac8a
Dustin Quam <quamsta@gmail.com>
2014-03-21 12:16:08 -0500

    renamed to php

--- division-bar/templates/DivisionBar.php
+++ division-bar/templates/DivisionBar.php
@@ -4,7 +4,7 @@
         <div class="division-directory clearfix">
             <div class="dosl-wrapper clearfix">
                 <a href="http://studentlife.uiowa.edu/" class="dosl">
-                    <img src="wp-content/themes/wrac/division-bar/images/division-bar/dosl-logo.png" alt="Division of Student Life">
+                    <img src="<?php echo get_template_directory_uri(); ?>/division-bar/images/division-bar/dosl-logo.png" alt="Division of Student Life">
                 </a>
                 <p class="adr">
                     The Division of Student Life fosters student success by creating and promoting inclusive educationally purposeful services and activities within and beyond the classroom.
@@ -38,11 +38,11 @@
         </div><!-- end .division-directory -->
         <div class="clearfix">
             <a href="http://www.uiowa.edu/" class="uiowa">
-                <img src="wp-content/themes/wrac/division-bar/images/division-bar/uiowa.png" alt="The University of Iowa" width="177">
+                <img src="<?php echo get_template_directory_uri(); ?>/division-bar/images/division-bar/uiowa.png" alt="The University of Iowa" width="177">
             </a>
 
             <a href="#" class="directory-toggle">
-                <img src="wp-content/themes/wrac/division-bar/images/division-bar/division_studentlife.png" alt="Division of Student Life" width="224">
+                <img src="<?php echo get_template_directory_uri(); ?>/division-bar/images/division-bar/division_studentlife.png" alt="Division of Student Life" width="224">
             </a>
 		    	<a href="#" class="search-toggle">Search</a>
         </div>
